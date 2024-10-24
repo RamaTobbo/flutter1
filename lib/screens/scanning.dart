92fbbec3eeb93047154ac042e17a80bb9da6bc8a
unknown <ramatobbo5@gmail.com>
2024-10-24 18:10:53 +0300

    Initial commit

--- lib/screens/scanning.dart
+++ lib/screens/scanning.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
-
 import 'package:track_pro/screens/BluetoothPairingSuccess.dart';
 
 class QrCodeScanner extends StatelessWidget {
@@ -12,27 +11,48 @@ class QrCodeScanner extends StatelessWidget {
   final Function setResult;
   final MobileScannerController controller = MobileScannerController();
 
+  final String expectedQRCodeValue = "track_pro";
+
   @override
   Widget build(BuildContext context) {
-    return MobileScanner(
-      controller: controller,
-      onDetect: (BarcodeCapture capture) async {
-        final List<Barcode> barcodes = capture.barcodes;
-        final barcode = barcodes.first;
+    return Stack(
+      children: [
+        MobileScanner(
+          controller: controller,
+          onDetect: (BarcodeCapture capture) async {
+            final List<Barcode> barcodes = capture.barcodes;
+            final barcode = barcodes.first;
 
-        if (barcode.rawValue != null) {
-          setResult(barcode.rawValue);
+            if (barcode.rawValue != null) {
+              if (barcode.rawValue == expectedQRCodeValue) {
+                setResult(barcode.rawValue);
 
-          await controller.stop();
-          controller.dispose();
+                await controller.stop();
+                controller.dispose();
 
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-                builder: (context) => const Bluetoothpairingsuccess()),
-          );
-        }
-      },
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const Bluetoothpairingsuccess()),
+                );
+              }
+            }
+          },
+        ),
+        Center(
+          child: Container(
+            width: 250,
+            height: 250,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.amber,
+                width: 5,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
