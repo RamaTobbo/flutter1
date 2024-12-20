da4520b5341e628dbb5c2660e44efa9c151f2399
unknown <ramatobbo5@gmail.com>
2024-12-20 21:42:40 +0200

    Initial commit

--- lib/provider/steps.dart
+++ lib/provider/steps.dart
@@ -1,98 +1,3 @@
-// import 'dart:async';
-// import 'dart:math';
-// import 'package:flutter/material.dart';
-// import 'package:sensors_plus/sensors_plus.dart';
-
-// class Steps extends ChangeNotifier {
-//   double _steps = 0;
-//   Timer? _timer;
-//   int _duration = 0;
-//   double _burnedCalories = 0;
-//   bool _isCounting = false;
-//   StreamSubscription<AccelerometerEvent>? _accelerometerSubscription;
-//   final double _threshold = 1.9;
-//   double _previousMagnitude = 0.0;
-//   double _stirdeLength = 0;
-//   double get steps => _steps;
-//   int get duration => _duration;
-//   double get burnedCalories => _burnedCalories;
-//   bool get isCounting => _isCounting;
-//   double get stirdeLength => _stirdeLength;
-
-//   void startCounting() {
-//     if (_isCounting) return;
-
-//     _isCounting = true;
-//     _accelerometerSubscription =
-//         accelerometerEvents.listen((AccelerometerEvent event) {
-//       double magnitude =
-//           sqrt(pow(event.x, 2) + pow(event.y, 2) + pow(event.z, 2));
-
-//       if ((magnitude - _previousMagnitude).abs() > _threshold) {
-//         _burnedCalories = _steps * 0.04;
-//         _stirdeLength = _steps * 0.64;
-//         _steps++;
-//         notifyListeners();
-//       }
-//       _previousMagnitude = magnitude;
-//     });
-//     _startTimer();
-//   }
-
-//   void _startTimer() {
-//     _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-//       _duration++;
-//       notifyListeners();
-//     });
-//   }
-
-//   void _stopTimer() {
-//     _timer?.cancel();
-//     _timer = null;
-//   }
-
-//   void pauseCounting() {
-//     if (!_isCounting) return; // If not counting, do nothing
-
-//     _accelerometerSubscription?.pause(); // Pause the accelerometer subscription
-//     _isCounting = false; // Update the state
-//     _stopTimer(); // Stop the timer
-//     notifyListeners(); // Notify listeners to update UI
-//   }
-
-//   // Resume counting process after pause
-//   void resumeCounting() {
-//     if (_isCounting) return; // If already counting, do nothing
-
-//     _accelerometerSubscription
-//         ?.resume(); // Resume the accelerometer subscription
-//     _isCounting = true; // Update the state to counting
-//     _startTimer(); // Restart the timer
-//     notifyListeners(); // Notify listeners to update UI
-//   }
-
-//   void stopCounting() {
-//     _accelerometerSubscription?.cancel();
-//     _isCounting = false;
-//     _stopTimer();
-//     notifyListeners();
-//   }
-
-//   void resetSteps() {
-//     _steps = 0;
-//     _duration = 0;
-//     _burnedCalories = 0;
-//     _previousMagnitude = 0.0;
-//     _stirdeLength = 0;
-
-//     notifyListeners();
-//   }
-
-//   void setSteps(double steps) {
-//     _steps = steps;
-//     notifyListeners();
-//   }
-// }
 import 'dart:async';
 import 'dart:math';
 import 'package:flutter/material.dart';
@@ -109,6 +14,9 @@ class Steps extends ChangeNotifier {
   final double _threshold = 1.9;
   double _previousMagnitude = 0.0;
   double _strideLength = 0;
+  DateTime? _lastPausedTime;
+  double _stepsBeforePause = 0.0;
+  double _magnitudeAtPause = 0.0;
 
   double get steps => _steps;
   int get duration => _duration;
@@ -122,19 +30,13 @@ class Steps extends ChangeNotifier {
 
     _isCounting = true;
     _isPaused = false;
+
+    // Start accelerometer subscription to track steps
     _accelerometerSubscription =
         accelerometerEvents.listen((AccelerometerEvent event) {
-      double magnitude =
-          sqrt(pow(event.x, 2) + pow(event.y, 2) + pow(event.z, 2));
-
-      if ((magnitude - _previousMagnitude).abs() > _threshold) {
-        _steps++;
-        _burnedCalories = _steps * 0.04;
-        _strideLength = _steps * 0.64;
-        notifyListeners();
-      }
-      _previousMagnitude = magnitude;
+      _handleStepDetection(event);
     });
+
     _startTimer();
   }
 
@@ -142,6 +44,10 @@ class Steps extends ChangeNotifier {
     if (!_isCounting || _isPaused) return;
 
     _isPaused = true;
+    _lastPausedTime = DateTime.now();
+    _stepsBeforePause = _steps;
+    _magnitudeAtPause = sqrt(pow(_previousMagnitude, 2));
+
     _accelerometerSubscription?.pause();
     _stopTimer();
     notifyListeners();
@@ -151,8 +57,14 @@ class Steps extends ChangeNotifier {
     if (!_isCounting || !_isPaused) return;
 
     _isPaused = false;
-    _accelerometerSubscription?.resume();
-    _startTimer();
+    _accelerometerSubscription?.resume(); // Resume accelerometer
+    _startTimer(); // Restart the timer
+
+    Future.delayed(Duration(milliseconds: 500), () {
+      _lastPausedTime = null;
+      _handleStepDetectionOnResume();
+    });
+
     notifyListeners();
   }
 
@@ -172,7 +84,7 @@ class Steps extends ChangeNotifier {
     _burnedCalories = 0;
     _strideLength = 0;
     _previousMagnitude = 0.0;
-
+    _lastPausedTime = null;
     notifyListeners();
   }
 
@@ -183,8 +95,38 @@ class Steps extends ChangeNotifier {
     });
   }
 
+  // Stop the timer
   void _stopTimer() {
     _timer?.cancel();
     _timer = null;
+  }
+
+  void _handleStepDetection(AccelerometerEvent event) {
+    double magnitude =
+        sqrt(pow(event.x, 2) + pow(event.y, 2) + pow(event.z, 2));
+
+    if (_lastPausedTime != null) {
+      return;
+    }
+
+    if ((magnitude - _previousMagnitude).abs() > _threshold) {
+      _steps++;
+      _burnedCalories = _steps * 0.04;
+      _strideLength = _steps * 0.64;
+      notifyListeners();
+    }
+
+    _previousMagnitude = magnitude;
+  }
+
+  void _handleStepDetectionOnResume() {
+    double magnitudeDifference =
+        sqrt(pow(_previousMagnitude, 2)) - _magnitudeAtPause;
+    if (magnitudeDifference.abs() > _threshold) {
+      _steps -= (_steps - _stepsBeforePause);
+      _burnedCalories = _steps * 0.04;
+      _strideLength = _steps * 0.64;
+      notifyListeners();
+    }
   }
 }
